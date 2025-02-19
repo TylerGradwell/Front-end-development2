c3c9b7c4d87b269c0fb543993779058d8d7b1726
Tyler Gradwell <l39596982@student.ua92.ac.uk>
2025-02-19 19:15:47 +0000

    Merge branch 'main' of https://github.com/TylerGradwell/Front-end-development2

--- docs/signup.js
+++ docs/signup.js
@@ -1,7 +1,8 @@
+//adds an event that triggers when form is submitted
 document.getElementById('signupForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+    event.preventDefault(); //prevents my page from refreshing automatically
     
     const email = document.getElementById('email').value;
     
     alert(`Welcome! Your account with email ${email} has been created.`);
-});
+});//gets value from input field and displays alert message
